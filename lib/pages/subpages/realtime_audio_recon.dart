cb6ec848ba50c6efa938aed65ca55904f33ba035
aibotez <2290227486@qq.com>
2024-03-15 21:44:32 +0800

    refl

--- lib/pages/subpages/realtime_audio_recon.dart
+++ lib/pages/subpages/realtime_audio_recon.dart
@@ -10,6 +10,8 @@ import 'package:flutter_sound_platform_interface/flutter_sound_recorder_platform
 import 'package:flutter_sound/flutter_sound.dart';
 import 'package:permission_handler/permission_handler.dart';
 
+import 'package:flutter/foundation.dart';
+
 
 class realtime_recon extends StatelessWidget {
   // const realtime_recon({super.key});
@@ -55,10 +57,20 @@ class realtime_recon extends StatelessWidget {
   }
 }
 
+
+
+
+
+
+
 class realtime_audio_recon_show extends StatelessWidget {
   // const realtime_audio_recon_show({super.key});
   FlutterSoundRecorder recorderModule = FlutterSoundRecorder();
-  FlutterSoundPlayer playerModule = FlutterSoundPlayer();
+  // FlutterSoundPlayer playerModule = FlutterSoundPlayer();
+
+  int _maxLength=6;
+  // FlautoRecorderPlugin.attachFlautoRecorder ( ctx, registrar.messenger ()  );
+  // TrackPlayerPlugin.attachTrackPlayer ( ctx, registrar.messenger ()  );
 
 
 
@@ -88,10 +100,7 @@ class realtime_audio_recon_show extends StatelessWidget {
       androidAudioFocusGainType: AndroidAudioFocusGainType.gain,
       androidWillPauseWhenDucked: true,
     ));
-    await playerModule.closePlayer();
-    await playerModule.openPlayer();
-    await playerModule
-        .setSubscriptionDuration(const Duration(milliseconds: 10));
+
   }
 
 
@@ -164,101 +173,21 @@ class realtime_audio_recon_show extends StatelessWidget {
           sampleRate: 8000,
         );
         /// 监听录音
-        _recorderSubscription = recorderModule.onProgress!.listen((e) {
+        var _recorderSubscription = recorderModule.onProgress!.listen((e) {
           var date = DateTime.fromMillisecondsSinceEpoch(
               e.duration.inMilliseconds,
               isUtc: true);
-          var txt = DateFormat('mm:ss:SS', 'en_GB').format(date);
           //设置了最大录音时长
           if (date.second >= _maxLength) {
             _stopRecorder();
             return;
           }
-          setState(() {
-            //更新录音时长
-            _recordText = txt.substring(1, 5);
-          });
+
         });
-        setState(() {
-          //更新录音状态和录音文件路径
-          _state = RecordPlayState.recording;
-          _path = path;
-        });
+
       });
     } catch (err) {
-      setState(() {
-        _stopRecorder();
-        _state = RecordPlayState.record;
-        _cancelRecorderSubscriptions();
-      });
-    }
-  }
-
-  /// 开始录音
-  _startRecorder() async {
-    try {
-      var status = await getPermissionStatus();
-
-      Directory tempDir = await getTemporaryDirectory();
-      var time = DateTime.now().millisecondsSinceEpoch;
-      String path =
-          '${tempDir.path}}-$time${ext[Codec.aacADTS.index]}' ;
-
-      print('===>  准备开始录音');
-      await recorderModule.startRecorder(
-          toFile: path,
-          codec: Codec.aacADTS,
-          bitRate: 8000,,
-          sampleRate: 8000,,
-          audioSource: AudioSource.microphone);
-      print('===>  开始录音');
-
-      /// 监听录音
-      recorderModule.onProgress!.listen((e) {
-        if (e != null && e.duration != null) {
-          DateTime date = new DateTime.fromMillisecondsSinceEpoch(
-              e.duration.inMilliseconds,
-              isUtc: true);
-
-          if (date.second >= _maxLength) {
-            print('===>  到达时常停止录音');
-            _stopRecorder();
-          }
-          setState(() {
-            print("时间：${date.second}");
-            print("当前振幅：${e.decibels}");
-            num = date.second;
-            setState(() {
-              if (e.decibels! > 0 && e.decibels! < 10) {
-                voiceIco = "Asset/message/icon_diyinliang.png";
-              } else if (e.decibels! > 20 && e.decibels! < 30) {
-                voiceIco = "Asset/message/icon_zhongyinliang.png";
-              } else if (e.decibels! > 30 && e.decibels! < 40) {
-                voiceIco = "Asset/message/icon_gaoyinliang.png";
-              } else if (e.decibels! > 40 && e.decibels! < 50) {
-                voiceIco = "Asset/message/icon_zhongyinliang.png";
-              } else if (e.decibels! > 50 && e.decibels! < 60) {
-                voiceIco = "Asset/message/icon_diyinliang.png";
-              } else if (e.decibels! > 70 && e.decibels! < 100) {
-                voiceIco = "Asset/message/icon_gaoyinliang.png";
-              } else {
-                voiceIco = "Asset/message/icon_diyinliang.png";
-              }
-            });
-          });
-        }
-      });
-      this.setState(() {
-        _state = RecordPlayState.recording;
-        _path = path;
-        print("path == $path");
-      });
-    } catch (err) {
-      setState(() {
-        print(err.toString());
-        _stopRecorder();
-        _state = RecordPlayState.record;
-      });
+      _stopRecorder();
     }
   }
 
@@ -266,71 +195,14 @@ class realtime_audio_recon_show extends StatelessWidget {
   _stopRecorder() async {
     try {
       await recorderModule.stopRecorder();
-      print('stopRecorder===> fliePath:$_path');
-      widget.stopRecord!(_path, num);
+      print('stopRecorder===> fliePath:');
     } catch (err) {
       print('stopRecorder error: $err');
     }
-    setState(() {
-      _state = RecordPlayState.play;
-    });
-  }
-  ///开始播放，这里做了一个播放状态的回调
-  void startPlayer(path, {Function(dynamic)? callBack}) async {
-    try {
-      if (path.contains('http')) {
-        await playerModule.startPlayer(
-            fromURI: path,
-            codec: Codec.mp3,
-            sampleRate: 44000,
-            whenFinished: () {
-              stopPlayer();
-              callBack!(0);
-            });
-      } else {
-        //判断文件是否存在
-        if (await _fileExists(path)) {
-          if (playerModule.isPlaying) {
-            playerModule.stopPlayer();
-          }
-          await playerModule.startPlayer(
-              fromURI: path,
-              codec: Codec.aacADTS,
-              sampleRate: 44000,
-              whenFinished: () {
-                stopPlayer();
-                callBack!(0);
-              });
-        } else {}
-      }
-      //监听播放进度
-      playerModule.onProgress!.listen((e) {});
-      callBack!(1);
-    } catch (err) {
-      callBack!(0);
-    }
   }
 
-  /// 结束播放
-  void stopPlayer() async {
-    try {
-      await playerModule.stopPlayer();
-    } catch (err) {}
-  }
 
-  ///获取播放状态
-  Future getPlayState() async {
-    return await playerModule.getPlayerState();
-  }
 
-  /// 释放播放器
-  void releaseFlauto() async {
-    try {
-      await playerModule.closePlayer();
-    } catch (e) {
-      print(e);
-    }
-  }
 
   /// 判断文件是否存在
   Future _fileExists(String path) async {
@@ -342,7 +214,7 @@ class realtime_audio_recon_show extends StatelessWidget {
     // TODO: implement dispose
     // super.dispose();
     recorderModule.closeRecorder();
-    playerModule.closePlayer();
+
   }
 
 
@@ -352,6 +224,10 @@ class realtime_audio_recon_show extends StatelessWidget {
 
     FlutterSoundRecorder recorderModule = FlutterSoundRecorder();
     FlutterSoundPlayer playerModule = FlutterSoundPlayer();
+
+    init();
+    _startRecorder();
+
 
     return const Placeholder();
   }
