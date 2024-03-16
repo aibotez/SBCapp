1d61c8a77270c89fe2998b32affc2ab9b801ae8a
aibotez <2290227486@qq.com>
2024-03-16 09:39:24 +0800

    refl

--- lib/pages/subpages/realtime_audio_recon.dart
+++ lib/pages/subpages/realtime_audio_recon.dart
@@ -60,14 +60,236 @@ class realtime_recon extends StatelessWidget {
 
 
 
+class realtime_audio_recon_show extends StatefulWidget {
+  const realtime_audio_recon_show({super.key});
+
+  @override
+  State<realtime_audio_recon_show> createState() => _realtime_audio_recon_showState();
+}
+
+class _realtime_audio_recon_showState extends State<realtime_audio_recon_show> {
+  FlutterSoundRecorder recorderModule = FlutterSoundRecorder();
+  // FlutterSoundPlayer playerModule = FlutterSoundPlayer();
+
+  bool start_record = false;
+  Widget start_record_icon = Icon(Icons.not_started_outlined, size: 30, color: Colors.blue,);
+
+  int _maxLength=6;
+  // FlautoRecorderPlugin.attachFlautoRecorder ( ctx, registrar.messenger ()  );
+  // TrackPlayerPlugin.attachTrackPlayer ( ctx, registrar.messenger ()  );
+
+
+
+  void init() async {
+//开启录音
+    await recorderModule.openRecorder();
+    //设置订阅计时器
+    await recorderModule
+        .setSubscriptionDuration(const Duration(milliseconds: 10));
+
+    //设置音频
+    final session = await AudioSession.instance;
+    await session.configure(AudioSessionConfiguration(
+      avAudioSessionCategory: AVAudioSessionCategory.playAndRecord,
+      avAudioSessionCategoryOptions:
+      AVAudioSessionCategoryOptions.allowBluetooth |
+      AVAudioSessionCategoryOptions.defaultToSpeaker,
+      avAudioSessionMode: AVAudioSessionMode.spokenAudio,
+      avAudioSessionRouteSharingPolicy:
+      AVAudioSessionRouteSharingPolicy.defaultPolicy,
+      avAudioSessionSetActiveOptions: AVAudioSessionSetActiveOptions.none,
+      androidAudioAttributes: const AndroidAudioAttributes(
+        contentType: AndroidAudioContentType.speech,
+        flags: AndroidAudioFlags.none,
+        usage: AndroidAudioUsage.voiceCommunication,
+      ),
+      androidAudioFocusGainType: AndroidAudioFocusGainType.gain,
+      androidWillPauseWhenDucked: true,
+    ));
+
+  }
+
+
+
+  Future<bool> getPermissionStatus() async {
+    Permission permission = Permission.microphone;
+    //granted 通过，denied 被拒绝，permanentlyDenied 拒绝且不在提示
+    PermissionStatus status = await permission.status;
+    if (status.isGranted) {
+      return true;
+    } else if (status.isDenied) {
+      requestPermission(permission);
+    } else if (status.isPermanentlyDenied) {
+      openAppSettings();
+    } else if (status.isRestricted) {
+      requestPermission(permission);
+    } else {}
+    return false;
+  }
+  ///申请权限
+  void requestPermission(Permission permission) async {
+    PermissionStatus status = await permission.request();
+    if (status.isPermanentlyDenied) {
+      openAppSettings();
+    }
+  }
+
+
+
+  /// 开始录音
+  _startRecorder() async {
+    try {
+      //获取麦克风权限
+      await getPermissionStatus().then((value) async {
+        if (!value) {
+          return;
+        }
+        //用户允许使用麦克风之后开始录音
+        // Directory tempDir = await getTemporaryDirectory();
+        // var time = DateTime.now().millisecondsSinceEpoch;
+        // String path = '${tempDir.path}/$time${ext[Codec.aacADTS.index]}';
+        // print(path);
+        var recordingDataController = StreamController<Food>();
+
+        recordingDataController.stream.listen((buffer) {
+          if (buffer is FoodData) {
+            List auddata0 = buffer.data!;
+
+            // List auddata = auddata0/32768.0;
+            print(auddata0);
+            // sink.add(buffer.data!);
+
+          }
+        });
+
+        //这里我录制的是aac格式的，还有其他格式
+        await recorderModule.startRecorder(
+
+          toStream: recordingDataController.sink,
+          // toFile: path,
+          codec: Codec.pcm16,
+          bitRate: 16000,
+          numChannels: 1,
+          sampleRate: 16000,
+          bufferSize:1600,
+        );
+        /// 监听录音
+        recorderModule.onProgress!.listen((e) {
+          var date = DateTime.fromMillisecondsSinceEpoch(
+
+              e.duration.inMilliseconds,
+              isUtc: true);
+          // print(e);
+          //设置了最大录音时长
+          if (date.second >= _maxLength) {
+            _stopRecorder();
+            return;
+          }
+
+        });
+
+      });
+    } catch (err) {
+      _stopRecorder();
+    }
+  }
+
+  /// 结束录音
+  _stopRecorder() async {
+    try {
+      await recorderModule.stopRecorder();
+      print('stopRecorder===> fliePath:');
+    } catch (err) {
+      print('stopRecorder error: $err');
+    }
+  }
 
 
 
 
-class realtime_audio_recon_show extends StatelessWidget {
+  /// 判断文件是否存在
+  Future _fileExists(String path) async {
+    return await File(path).exists();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    recorderModule.closeRecorder();
+
+  }
+
+  
+
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    // FlutterSoundRecorder recorderModule = FlutterSoundRecorder();
+    // FlutterSoundPlayer playerModule = FlutterSoundPlayer();
+
+    init();
+    // _startRecorder();
+
+    void start_close_record(){
+      if (start_record){
+        start_record = false;
+        start_record_icon = Icon(Icons.not_started_outlined, size: 30, color: Colors.blue,);
+        _stopRecorder();
+
+      } else{
+        start_record = true;
+        start_record_icon = Icon(Icons.stop_circle, size: 30, color: Colors.red,);
+        _startRecorder();
+      }
+
+      setState(() {});
+
+    }
+
+
+    return Container(
+      // margin: const EdgeInsets.only(top:10),
+      child: Column(
+        children: [
+
+          Container(
+            margin: const EdgeInsets.only(left:40),
+            child: Row(children: [
+              GestureDetector(child: Container(
+                child: Row(
+                  children: [
+                    start_record_icon,
+                  ],
+                ),
+              ),onTap: (){start_close_record();},),
+
+              Container(
+                margin: const EdgeInsets.only(left:20.0),
+                child: Text('..',
+                  style: TextStyle(color: Colors.red,fontSize: 20),
+                ),
+              ),
+            ],),
+          ),
+          Divider(indent: 0,endIndent: 0,),
+          Container(),
+        ],
+      ),
+    );
+  }
+}
+
+
+
+class realtime_audio_recon_show1 extends StatelessWidget {
   // const realtime_audio_recon_show({super.key});
   FlutterSoundRecorder recorderModule = FlutterSoundRecorder();
   // FlutterSoundPlayer playerModule = FlutterSoundPlayer();
+
+  bool start_record = false;
 
   int _maxLength=6;
   // FlautoRecorderPlugin.attachFlautoRecorder ( ctx, registrar.messenger ()  );
@@ -224,10 +446,54 @@ class realtime_audio_recon_show extends StatelessWidget {
     // FlutterSoundPlayer playerModule = FlutterSoundPlayer();
 
     init();
-    _startRecorder();
+    // _startRecorder();
+
+    void start_close_record(){
+      if (start_record){
+        start_record = false;
+        _stopRecorder();
+
+      } else{
+        start_record = true;
+        _startRecorder();
+      }
+
+    }
 
 
-    return const Placeholder();
+    return Container(
+      // margin: const EdgeInsets.only(top:10),
+      child: Column(
+        children: [
+
+          Container(
+            margin: const EdgeInsets.only(left:40),
+            child: Row(children: [
+              GestureDetector(child: Container(
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.not_started_outlined,
+                      size: 30,
+                      color: Colors.blue,
+                    ),
+                  ],
+                ),
+              ),onTap: (){start_close_record();},),
+
+              Container(
+                margin: const EdgeInsets.only(left:20.0),
+                child: Text('..',
+                  style: TextStyle(color: Colors.red,fontSize: 20),
+                ),
+              ),
+            ],),
+          ),
+          Divider(indent: 0,endIndent: 0,),
+          Container(),
+        ],
+      ),
+    );
   }
 }
 
